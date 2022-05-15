5166e03cb720ed164338140e110c90227472d6a4
Francis N. Numbi <francisnnumbi@yandex.com>
2022-05-15 23:32:28 +0200

    working on contributors list and correction of display errors

--- lib/system/configs/configs.dart
+++ lib/system/configs/configs.dart
@@ -1,6 +1,6 @@
 import 'package:get/get.dart';
 
-const bool DEBUG_MODE = true;
+const bool DEBUG_MODE = false;
 final String APP_NAME = "Tabwa French Dictionnary".tr;
 const String APP_AUTHOR = "Francis N Numbi";
 
